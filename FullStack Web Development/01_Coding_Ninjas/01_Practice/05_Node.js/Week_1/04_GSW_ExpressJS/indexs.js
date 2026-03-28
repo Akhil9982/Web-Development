30a21a71eb52bbf69334561aedd1e406245a229d
Akhil Battula <b_akhilkumar@outlook.com>
2026-03-28 19:01:29 +0530

    Added new files

---
+++ FullStack Web Development/01_Coding_Ninjas/01_Practice/05_Node.js/Week_1/04_GSW_ExpressJS/indexs.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+// server
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Welcome to express");
+});
+
+server.use(express.static("public"));
+
+server.listen(3300);
+console.log("Server is listening on http://localhost:3300");
